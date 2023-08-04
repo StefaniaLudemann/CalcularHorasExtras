c17652ff193c77458e9c86dc9e5689adefc03564
Steffi Lude <137961894+SteffiLude@users.noreply.github.com>
2023-08-03 23:03:03 -0300

    proyecto en csharp

---
+++ calcular-horas-extras/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calcular_horas_extras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal horas = 0m;
+            decimal sueldo = 0m;
+            decimal categoria = 0m;
+            decimal horaTrabajada = 0m;
+            decimal totalSueldo = 0m;
+
+            Console.WriteLine("Categoría 1 hasta 30 hs extras cobra $10 la hs.");
+            Console.WriteLine("Categoría 2 hasta 30 hs extras cobra $14 la hs.");
+            Console.WriteLine("Categoría 1 mas de 30 hs extras cobra $11 la hs.");
+            Console.WriteLine("Categoría 2 mas de 30 hs extras cobra $20 la hs.");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Console.WriteLine("por favor ingrese las horas trabajadas: ");
+            horas = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("por favor ingrese su sueldo: ");
+            sueldo = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("por favor ingrese su categoria: ");
+            categoria = decimal.Parse(Console.ReadLine());
+
+            if(categoria==1 && horas <= 30)
+            {
+                horaTrabajada = horas * 10;
+                totalSueldo = sueldo + horaTrabajada;
+                Console.WriteLine("usted cobrará "+totalSueldo + " pesos.");
+            }else if(categoria==1 && horas > 30)
+            {
+                horaTrabajada = horas * 11;
+                totalSueldo = sueldo + horaTrabajada;
+                Console.WriteLine("usted cobrará " + totalSueldo + " pesos.");
+            }else if(categoria==2 && horas <= 30)
+            {
+                horaTrabajada = horas * 14;
+                totalSueldo = sueldo + horaTrabajada;
+                Console.WriteLine("usted cobrará " + totalSueldo + " pesos.");
+            }else if(categoria==2 && horas > 30)
+            {
+                horaTrabajada = horas * 20;
+                totalSueldo = sueldo + horaTrabajada;
+                Console.WriteLine("usted cobrará " + totalSueldo+ " pesos.");
+            }
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ calcular-horas-extras/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("calcular-horas-extras")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("calcular-horas-extras")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("9de78f4d-8ca4-409a-81d0-1a1e2349d75c")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
